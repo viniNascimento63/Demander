73ead21b30d60df130f4f2e869051c7714a9cc28
Vinícius Nascimento <vini_sn@outlook.com>
2024-08-06 11:33:59 -0300

    🖌️style

--- conversor_numeros/resource/js/script.js
+++ conversor_numeros/resource/js/script.js
@@ -1,10 +1,6 @@
 const inputRomanos = document.querySelector("#romanos");
 const num = document.querySelector('#indo');
 
-if (num.textContent >= 10000) {
-    num.classList.add('overline');
-}
-
 inputRomanos.addEventListener("keypress", (e) => {
     const keyCode =  (e.keyCode ? e.keyCode : e.wich);
     console.log(keyCode);
